1adc3bbce94857ba3f283aa5a1f0993aba694309
Sneha Rani <97622386+sneha-2510@users.noreply.github.com>
2022-01-22 17:12:04 +0530

    Update multiplicationTable.java

--- multiplicationTable.java
+++ multiplicationTable.java
@@ -6,7 +6,7 @@ public class multiplicationTable {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the no whose multiplication table has to be wirtten:-");
+        System.out.print("Enter the no whose multiplication table has to be written:-");
         int n = sc.nextInt();
         System.out.println("Multiplication table of "+ n);
         for(int i = 1;i<=10;i++){
